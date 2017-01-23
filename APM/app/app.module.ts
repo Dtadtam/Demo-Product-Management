0015968be00c319a50647cf85dfba760b4c97874
CheawBakka <cyberutimate@gmail.com>
2017-01-23 14:36:22 +0700

    Create APM Project

---
+++ APM/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent }  from './app.component';
+import { ProductListComponent } from './product/product-list.component';
+import { ProductFilterPipe } from './product/product-filter.pipe';
+import { StarComponent } from './shared/star.component';
+
+@NgModule({
+  imports: [ 
+    BrowserModule,
+    FormsModule
+  ],
+  declarations: [ 
+    AppComponent,
+    ProductListComponent,
+    ProductFilterPipe,
+    StarComponent
+  ],
+  bootstrap: [ AppComponent ]
+})
+export class AppModule { }
